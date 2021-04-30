243c4a98f0cd06dc20a89ab6d8515d200c464f76
dairedo7 <vladik719@gmail.com>
2021-04-30 19:56:53 +0200

    Update_for_mail_fields

--- reg_form/mail.php
+++ reg_form/mail.php
@@ -1,4 +1,5 @@
 <?php
+
    $name = $_POST['name']; 
    $email = $_POST['email'];
    $country = $_POST['country'];
@@ -7,7 +8,20 @@
 
    $subject = "=?utf-8?B?".base64_encode("VINTURION - Сообщение от клиента!")."?=";
    $headers = "From: $email\r\nReply-to: $email\r\nContent-type: text/html; charset=utf-8\r\n";
+   
 
-   $success = mail("vladik719@gmail.com", $subject, $message, $headers);
+
+      $message = "Ім'я:".$name."\r\n"."Email: ".$email."\r\n"."Країна: ".$country."\r\n"."Телефон: ".$phone."\r\n"."Деталі заявки: ".$message."\r\n";
+      // $message .= "Email:".$email."\r\n";
+      // $message .= "Country: ".$country "\r\n";
+      // $message .= "Phone: ".$phone "\r\n";
+      // $text = str_replace($message, "\n", $name);
+      $to = 'vladik719@gmail.com';
+
+   $success = mail($to, $subject, $message, $headers);
    echo $success;
+
+
+
+
 ?>
